4f144dc301b352c106baebb8f35cc87120772c1e
fipso <fipso@noreply.users.github.com>
2025-04-26 13:28:42 +0200

    chore: remove poc bash scripts

--- backend/poc/user.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cat msg.json | jq '.data.user.result.timeline.timeline.instructions[] | .entries[] | select(has("content")) | .content | select(has("items")) | .items[0].item.itemContent.tweet_results.result.core.user_results.result'
