fa623c2be0b10117bfb705ee19beeec9649bd46f
fipso <fipso@users.noreply.github.com>
2025-04-26 15:51:00 +0200

    feat: ask backend for tweet topic

--- src/utils/filter/shouldFilterPost.ts
+++ src/utils/filter/shouldFilterPost.ts
@@ -1,5 +1,31 @@
 import { IPostInfo } from "../../interfaces/IPostInfo";
 
-export const shouldFilterPost = (post: IPostInfo): boolean => {
+import { filterSettings } from "../../functions/content";
+
+export const shouldFilterPost = async (post: IPostInfo): Promise<boolean> => {
+  // Ask the backend what topic score the post has
+
+  const res = await fetch("http://root.fipso.dev:3000/analyze-tweets", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      tweets: [
+        {
+          id: post.id,
+          text: post.content,
+        },
+      ],
+    })
+  });
+  const resData = await res.json();
+
+  if (filterSettings.political) {
+    if (resData.tweets[post.id].Political > 5) {
+      return true;
+    }
+  }
+
   return false;
 }
