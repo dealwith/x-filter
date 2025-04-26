be718b9e51fc34df1b1f8b5e7fc2a2917939a940
dealwith <gleb.krishin@icloud.com>
2025-04-26 17:43:57 +0200

    refactor: comment out backend fetch logic in shouldFilterPost function

--- src/utils/filter/shouldFilterPost.ts
+++ src/utils/filter/shouldFilterPost.ts
@@ -17,26 +17,28 @@ export const shouldFilterPost = async (post: IPostInfo): Promise<boolean> => {
 
   // Ask the backend what topic score the post has
   let resData;
+
   try {
-    const res = await fetch("http://root.fipso.dev:3000/analyze-tweets", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        tweets: [
-          {
-            id: post.id,
-            text: post.content,
-          },
-        ],
-      }),
-    });
-    resData = await res.json();
+    // const res = await fetch("http://root.fipso.dev:3000/analyze-tweets", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     tweets: [
+    //       {
+    //         id: post.id,
+    //         text: post.content,
+    //       },
+    //     ],
+    //   }),
+    // });
+    // resData = await res.json();
   } catch (error) {
     console.error("Error fetching or parsing response:", error);
-    return false; // Default to not filtering the post if an error occurs
+    return false;
   }
+
   if (filterSettings.political) {
     if (resData.tweets[post.id].Political > 5) {
       return true;
