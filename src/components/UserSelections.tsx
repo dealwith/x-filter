6ab1e48193d75372b9c748796e6c9ffb13b12760
Nicholas Jones <njonesgrad@gmail.com>
2025-04-26 11:58:18 +0200

    Frontend UI + Add Boostrap

---
+++ src/components/UserSelections.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const UserSelections = () => {
+  return (
+    <>
+      <Navbar bg="dark text-white" expand="lg">
+        <Container>
+          <Navbar.Brand  className="text-white" href="#">X Filter</Navbar.Brand>
+          <Form className="ms-auto">
+            <Form.Check
+              type="switch"
+              id="navbar-switch"
+              label="On / Off"
+            />
+          </Form>
+        </Container>
+      </Navbar>
+
+      <Container className="mt-4">
+        <Row>
+          <Col xs="auto">
+            <Form>
+              <Form.Check
+                type="switch"
+                id="body-switch-1"
+                label="Ads"
+              />
+              <Form.Check
+                type="switch"
+                id="body-switch-2"
+                label="Something Else"
+              />
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default UserSelections;
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./popup.scss"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UserSelections from "./components/UserSelections";
 
 const Popup = () => {
   return (
     <>
-      <div id="popup-window"/>
+      <div id="popup-window">
+        <UserSelections/>
+      </div>
     </>
   );
 };
